29d785897264cf14cfccf86078258bf8287fdf65
Christian Henkel <christian.henkel2@de.bosch.com>
2024-06-14 11:06:07 +0200

    empty files for future implementations
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

---
+++ scxml_converter/test/test_systemtest_scxml_entries.py
@@ -0,0 +1,24 @@
+
+
+# We want to support ..
+
+# - scxml
+#     - state
+#         - onentry
+#             - {executable content}
+#         - onexit
+#             - {executable content}
+#         - transition
+#             - {executable content}
+#     - datamodel
+#         - data
+
+# executable content
+# - send
+#     - param
+# - if / elseif / else
+# - assign 
+
+# dump to scxml file
+# (read scxml file)
+
