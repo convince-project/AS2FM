c36941e3ca801e638e800a45e8fac9a1c4365a24
Christian Henkel <christian.henkel2@de.bosch.com>
2024-06-18 15:16:26 +0200

    moved test folder
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- scxml_converter/test/test_systemtest_scxml_entries.py
+++ scxml_converter/test/test_systemtest_scxml_entries.py
@@ -15,9 +15,11 @@
 
 import os
 from xml.etree import ElementTree as ET
+
+from scxml_converter.scxml_entries import (ScxmlAssign, ScxmlDataModel,
+                                           ScxmlParam, ScxmlRoot, ScxmlSend,
+                                           ScxmlState, ScxmlTransition)
 from test_utils import canonicalize_xml, remove_empty_lines
-from scxml_converter.scxml_entries import ScxmlRoot, ScxmlDataModel, ScxmlState, ScxmlSend, ScxmlParam, \
-    ScxmlTransition, ScxmlAssign
 
 
 def test_battery_drainer_from_code():
@@ -44,16 +46,18 @@ def test_battery_drainer_from_code():
 """
     battery_drainer_scxml = ScxmlRoot("BatteryDrainer")
     battery_drainer_scxml.set_data_model(ScxmlDataModel([("battery_percent", "100")]))
-    use_battery_state = ScxmlState("use_battery",
-                                   on_entry=[ScxmlSend("ros_topic.level",
-                                                       [ScxmlParam("data", expr="battery_percent")])],
-                                   body=[ScxmlTransition("use_battery", ["ros_time_rate.my_timer"],
-                                                         body=[ScxmlAssign("battery_percent", "battery_percent - 1")]),
-                                         ScxmlTransition("use_battery", ["ros_topic.charge"],
-                                                         body=[ScxmlAssign("battery_percent", "100")])])
+    use_battery_state = ScxmlState(
+        "use_battery",
+        on_entry=[ScxmlSend("ros_topic.level",
+                            [ScxmlParam("data", expr="battery_percent")])],
+        body=[ScxmlTransition("use_battery", ["ros_time_rate.my_timer"],
+                              body=[ScxmlAssign("battery_percent", "battery_percent - 1")]),
+              ScxmlTransition("use_battery", ["ros_topic.charge"],
+                              body=[ScxmlAssign("battery_percent", "100")])])
     battery_drainer_scxml.add_state(use_battery_state, initial=True)
     # Check output xml
-    ref_file = os.path.join(os.path.dirname(__file__), '_test_data', 'expected_output', 'battery_drainer.scxml')
+    ref_file = os.path.join(os.path.dirname(__file__), '_test_data',
+                            'expected_output_ros_to_scxml', 'battery_drainer.scxml')
     assert os.path.exists(ref_file), f"Cannot find ref. file {ref_file}."
     with open(ref_file, 'r', encoding='utf-8') as f_o:
         expected_output = f_o.read()
