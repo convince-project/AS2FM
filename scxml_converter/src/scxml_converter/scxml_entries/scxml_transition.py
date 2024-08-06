fd4ea5870782d6fdb840f45c6119e58d7f26f3dc
Christian Henkel <christian.henkel2@de.bosch.com>
2024-08-06 18:29:03 +0200

    scxml_transition.py done
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- scxml_converter/src/scxml_converter/scxml_entries/scxml_transition.py
+++ scxml_converter/src/scxml_converter/scxml_entries/scxml_transition.py
@@ -40,8 +40,8 @@ class ScxmlTransition(ScxmlBase):
             f"Error: SCXML transition: XML root tag name is not {ScxmlTransition.get_tag_name()}."
         target = xml_tree.get("target")
         assert target is not None, "Error: SCXML transition: target attribute not found."
-        events = xml_tree.get("event")
-        events = events.split(" ") if events is not None else None
+        events_str = xml_tree.get("event")
+        events = events_str.split(" ") if events_str is not None else []
         condition = xml_tree.get("cond")
         exec_body = execution_body_from_xml(xml_tree)
         exec_body = exec_body if exec_body is not None else None
@@ -69,7 +69,7 @@ class ScxmlTransition(ScxmlBase):
             body), "Error SCXML transition: invalid body provided."
         self._target = target
         self._body = body
-        self._events = events
+        self._events = events if events is not None else []
         self._condition = condition
 
     def get_target_state_id(self) -> str:
@@ -89,8 +89,6 @@ class ScxmlTransition(ScxmlBase):
         return self._body if self._body is not None else []
 
     def add_event(self, event: str):
-        if self._events is None:
-            self._events = []
         self._events.append(event)
 
     def append_body_executable_entry(self, exec_entry: ScxmlExecutableEntry):
