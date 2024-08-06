f1174863ffc0cbe2ef0a434a4a2ddd33d8fb9f3c
Christian Henkel <christian.henkel2@de.bosch.com>
2024-08-06 17:23:11 +0200

    sorting init (again)
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- scxml_converter/src/scxml_converter/scxml_entries/__init__.py
+++ scxml_converter/src/scxml_converter/scxml_entries/__init__.py
@@ -1,8 +1,8 @@
 from .scxml_base import ScxmlBase  # noqa: F401
 from .scxml_data import ScxmlData  # noqa: F401
 from .scxml_data_model import ScxmlDataModel  # noqa: F401
-from .scxml_executable_entries import (ScxmlAssign,  # noqa: F401
-                                       ScxmlExecutableEntry,
+from .scxml_executable_entries import ScxmlAssign  # noqa: F401
+from .scxml_executable_entries import (ScxmlExecutableEntry,
                                        ScxmlExecutionBody, ScxmlIf, ScxmlSend,
                                        as_plain_execution_body,
                                        execution_body_from_xml,
@@ -10,11 +10,11 @@ from .scxml_executable_entries import (ScxmlAssign,  # noqa: F401
                                        valid_execution_body)
 from .scxml_param import ScxmlParam  # noqa: F401
 from .scxml_root import ScxmlRoot  # noqa: F401
-from .scxml_ros_entries import (ScxmlRosDeclarations,  # noqa: F401
-                                ScxmlRosSends, ScxmlRosTransitions)
+from .scxml_ros_entries import ScxmlRosDeclarations  # noqa: F401
+from .scxml_ros_entries import ScxmlRosSends, ScxmlRosTransitions
 from .scxml_ros_field import RosField  # noqa: F401
-from .scxml_ros_service import (RosServiceClient,  # noqa: F401
-                                RosServiceHandleRequest,
+from .scxml_ros_service import RosServiceClient  # noqa: F401
+from .scxml_ros_service import (RosServiceHandleRequest,
                                 RosServiceHandleResponse,
                                 RosServiceSendRequest, RosServiceSendResponse,
                                 RosServiceServer)
