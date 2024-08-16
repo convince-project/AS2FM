42b7b57f8b5c22af93c5fc79b412391198a41461
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2024-08-16 11:00:13 +0200

    Lint
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- scxml_converter/test/test_systemtest_scxml_entries.py
+++ scxml_converter/test/test_systemtest_scxml_entries.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 import os
 
 from test_utils import canonicalize_xml, remove_empty_lines
