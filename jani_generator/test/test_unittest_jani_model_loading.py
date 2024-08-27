5bbe6d82681871b2b3f9933af68668ebf8fc8ffe
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2024-08-27 16:51:16 +0200

    Generalize properties loading and printing in jani_entries
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- jani_generator/test/test_unittest_jani_model_loading.py
+++ jani_generator/test/test_unittest_jani_model_loading.py
@@ -22,13 +22,13 @@ from jani_generator.jani_entries import JaniModel
 
 def test_jani_file_loading():
     jani_file = os.path.join(os.path.dirname(__file__),
-                             '_test_data', 'plain_jani_examples', 'example_arrays.jani')
+                             '_test_data', 'plain_jani_examples', 'array_test.jani')
     with open(jani_file, "r", encoding='utf-8') as file:
         convince_jani_json = json.load(file)
     jani_model = JaniModel.from_dict(convince_jani_json)
     assert isinstance(jani_model, JaniModel)
-    assert jani_model.get_name() == "example_arrays"
+    assert jani_model.get_name() == "array_example"
     assert "arrays" in jani_model.get_features()
-    assert len(jani_model.get_variables()) == 11
+    assert len(jani_model.get_variables()) == 2
     assert len(jani_model.get_constants()) == 0
-    assert len(jani_model.get_automata()) == 14
+    assert len(jani_model.get_automata()) == 1
