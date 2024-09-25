e81ef794c37066ca6f6bf889f3b0406aa1cf1c94
Marco Lampacrescia <65171491+MarcoLm993@users.noreply.github.com>
2024-09-25 18:19:36 +0200

    Hotfix support bt ports in handle goal (#34)
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- scxml_converter/src/scxml_converter/scxml_entries/scxml_ros_action_client.py
+++ scxml_converter/src/scxml_converter/scxml_entries/scxml_ros_action_client.py
@@ -124,6 +124,15 @@ class RosActionHandleGoalResponse(ScxmlTransition):
                                            self._reject_target)
         return valid_name and valid_accept and valid_reject
 
+    def instantiate_bt_events(self, _: str):
+        # We do not expect a body with BT ports to be substituted
+        pass
+
+    def update_bt_ports_values(self, _) -> None:
+        """Update the values of potential entries making use of BT ports."""
+        # We do not expect a body with BT ports to be substituted
+        pass
+
     def check_valid_ros_instantiations(self,
                                        ros_declarations: ScxmlRosDeclarationsContainer) -> bool:
         assert isinstance(ros_declarations, ScxmlRosDeclarationsContainer), \
