fd9236751ad59fb457f06f41c81910b8e558e3bb
Christian Henkel <hec2le@RNG-C-002AJ.rng.de.bosch.com>
2024-09-25 16:58:22 +0200

    colors and tests
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- visualizers/trace_visualizer/test/visualizer_test.py
+++ visualizers/trace_visualizer/test/visualizer_test.py
@@ -17,6 +17,35 @@ from trace_visualizer.visualizer import Traces
 
 import os
 
+
 def test_traces():
-    test_file = os.path.join(os.path.dirname(__file__), '_test_data', 'traces.csv')
-    traces = Traces(test_file)
+    data_prefix: str = 'ros_example_w_bt_battery_below_20_p_0_0107'
+    test_data_folder = os.path.join(
+        os.path.dirname(__file__),
+        '_test_data')
+    csv_file = os.path.join(test_data_folder, f'{data_prefix}.csv')
+    traces = Traces(csv_file)
+    ver, fal = traces.print_info_about_result()
+    assert ver == 73, \
+        f"The id of the first verified trace must be 73 but is {ver}."
+    assert fal == 0, \
+        f"The id of the first falsified trace must be 0 but is {fal}."
+
+    # Comparing the generated images with the reference images
+    output_file = 'test.png'
+    for i, fname_expected in [
+        (ver, f'{data_prefix}_verified.png'),
+            (fal, f'{data_prefix}_falsified.png')]:
+        assert not os.path.exists(output_file), \
+            f'The file {output_file} already exists.'
+        traces.write_trace_to_img(i, output_file)
+        assert os.path.exists(output_file), \
+            f'The file {output_file} was not created.'
+        path_expected = os.path.join(
+            test_data_folder,
+            'expected_output',
+            fname_expected)
+        with open(output_file, 'rb') as f1, open(path_expected, 'rb') as f2:
+            assert f1.read() == f2.read(), \
+                f'The content for {fname_expected} is not as expected.'
+        os.remove(output_file)
