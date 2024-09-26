f96797cb310ecd8e1ba27e1ccc5d6f6825653182
Christian Henkel <christian.henkel2@de.bosch.com>
2024-09-26 12:27:03 +0200

    testing with additional file
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- visualizers/jani_visualizer/test/jani_visualizer_test.py
+++ visualizers/jani_visualizer/test/jani_visualizer_test.py
@@ -19,26 +19,26 @@ import os
 import json
 
 
-def test_traces():
-    data_prefix: str = 'demo_manual'
-    test_data_folder = os.path.join(
-        os.path.dirname(__file__),
-        '_test_data')
-    jani_fname = os.path.join(test_data_folder, f'{data_prefix}.jani')
+def test_plantumlautomata():
+    for data_prefix in ['demo_manual', 'ros_example_w_bt']:
+        test_data_folder = os.path.join(
+            os.path.dirname(__file__),
+            '_test_data')
+        jani_fname = os.path.join(test_data_folder, f'{data_prefix}.jani')
 
-    with open(jani_fname, 'r', encoding='utf-8') as f:
-        jani_dict = json.load(f)
-    pua = PlantUMLAutomata(jani_dict)
-    puml_str = pua.to_plantuml(
-        with_assignments=True,  # default
-        with_guards=True,  # default
-        with_syncs=True  # default
-    )
+        with open(jani_fname, 'r', encoding='utf-8') as f:
+            jani_dict = json.load(f)
+        pua = PlantUMLAutomata(jani_dict)
+        puml_str = pua.to_plantuml(
+            with_assignments=True,  # default
+            with_guards=True,  # default
+            with_syncs=True  # default
+        )
 
-    # Comparing the generated images with the reference images
-    output_file = os.path.join(
-        test_data_folder, 'expected_output', f'{data_prefix}.plantuml')
-    with open(output_file, 'r', encoding='utf-8') as f:
-        expected_content = f.read()
-    assert puml_str == expected_content, \
-        f'The content for {output_file} is not as expected.'
+        # Comparing the generated images with the reference images
+        output_file = os.path.join(
+            test_data_folder, 'expected_output', f'{data_prefix}.plantuml')
+        with open(output_file, 'r', encoding='utf-8') as f:
+            expected_content = f.read()
+        assert puml_str == expected_content, \
+            f'The content for {output_file} is not as expected.'
