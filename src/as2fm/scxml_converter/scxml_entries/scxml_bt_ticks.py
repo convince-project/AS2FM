8bd28edcc737d11c82a1ca0b67fa063774467a5b
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2024-10-09 09:48:25 +0200

    STart implementing new scxml bt tags
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

---
+++ src/as2fm/scxml_converter/scxml_entries/scxml_bt_ticks.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2024 - for information on the respective copyright owner
+# see the NOTICE file
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+SCXML entries related to Behavior Tree Ticks and related responses.
+"""
+
+from typing import Optional, Union
+
+from lxml import etree as ET
+
+from as2fm.scxml_converter.scxml_entries import ScxmlSend, ScxmlTransition
+from as2fm.scxml_converter.scxml_entries.xml_utils import assert_xml_tag_ok, get_xml_argument
+
+
+class BtTick(ScxmlTransition):
+    """
+    Tick a BT plugin/control node, triggering the related transition.
+    """
+
+    @staticmethod
+    def get_tag_name() -> str:
+        return "bt_tick"
+
+    @staticmethod
+    def from_xml_tree(xml_tree: ET.Element) -> "BtTick":
+        assert_xml_tag_ok(BtTick, xml_tree)
+        target: str = get_xml_argument(BtTick, xml_tree, "target")
+        condition: Optional[str] = get_xml_argument(BtTick, xml_tree, "cond", none_allowed=True)
+        return BtTick(target, condition)
+
+    def __init__(self, target: str, condition: Optional[str] = None):
+        super().__init__(target, ["bt_tick"], condition)
+
+    def as_xml(self) -> ET.Element:
+        xml_bt_tick = ET.Element(BtTick.get_tag_name(), {"target": self._target})
+        if self._condition is not None:
+            xml_bt_tick.set("cond", self._condition)
+        return xml_bt_tick
+
+
+class BtTickChild(ScxmlSend):
+    """Tick one child of a control node."""
+
+    @staticmethod
+    def get_tag_name() -> str:
+        return "bt_tick_child"
+
+    @staticmethod
+    def from_xml_tree(xml_tree: ET.Element) -> "BtTickChild":
+        assert_xml_tag_ok(BtTickChild, xml_tree)
+        child_id: str = get_xml_argument(BtTickChild, xml_tree, "id")
+        return BtTickChild(child_id)
+
+    def __init__(self, child_id: Union[str, int]):
+        assert isinstance(
+            child_id, (str, int)
+        ), f"Error: SCXML BT Tick Child: invalid child id type {type(child_id)}."
+        self._child = child_id
+
+    def as_xml(self) -> ET.Element:
+        xml_bt_tick_child = ET.Element(BtTickChild.get_tag_name(), {"id": str(self._child)})
+        return xml_bt_tick_child
