8f3103aac4c33939f9b0b1d3c6210451f9ee6407
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2024-11-12 11:59:36 +0100

    Prevent ticking non-running BT trees, unless specified
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- test/scxml_converter/test_systemtest_xml.py
+++ test/scxml_converter/test_systemtest_xml.py
@@ -51,7 +51,7 @@ def bt_to_scxml_test(
     test_data_path = os.path.join(os.path.dirname(__file__), "_test_data", test_folder)
     bt_file = os.path.join(test_data_path, bt_file)
     plugin_files = [os.path.join(test_data_path, f) for f in bt_plugins]
-    scxml_objs = bt_converter(bt_file, plugin_files, 1.0)
+    scxml_objs = bt_converter(bt_file, plugin_files, 1.0, False)
     if store_generated:
         clear_output_folder(test_folder)
         for scxml_obj in scxml_objs:
