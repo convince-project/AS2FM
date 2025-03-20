c86eb94d999fa1f568c3be86df52e151bafef4de
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2025-03-20 10:06:16 +0100

    Fix documentation quotes
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- test/as2fm_common/test_unittest_ecmascript_interpretation.py
+++ test/as2fm_common/test_unittest_ecmascript_interpretation.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" "Test the SCXML data conversion"""
+"""Test the SCXML data conversion"""
 
 import unittest
 from array import array
