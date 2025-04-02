23b212e580129bd82326ec01496a318a0f80d246
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2025-04-02 13:41:21 +0200

    Robot model change
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- test/jani_generator/test_systemtest_scxml_to_jani.py
+++ test/jani_generator/test_systemtest_scxml_to_jani.py
@@ -465,10 +465,8 @@ class TestConversion(unittest.TestCase):
             "uc1_docking",
             model_xml="main_with_problem.xml",
             property_name="charging_starts",
-            expected_result_probability=0.01,
-            result_probability_tolerance=0.01,
+            expected_result_probability=0.0,
             trace_length_limit=1_000_000,
-            n_traces_limit=1_000,
         )
 
     def test_uc2_assembly_trigger_recovery(self):
