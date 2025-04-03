c6285ab8743ab5cd90de8c35bf85d64ea0cc9c68
Christian Henkel <christian.henkel2@de.bosch.com>
2025-04-03 15:33:28 +0200

    start of data types
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- src/as2fm/as2fm_common/ecmascript_interpretation.py
+++ src/as2fm/as2fm_common/ecmascript_interpretation.py
@@ -27,17 +27,15 @@ from as2fm.as2fm_common.common import ValidScxmlTypes
 BasicJsTypes = Union[int, float, bool, str]
 
 
-def interpret_ecma_script_expr(
-    expr: str, variables: Optional[Dict[str, ValidScxmlTypes]] = None
-) -> ValidScxmlTypes:
+def _interpret_ecmascript_expr(
+    expr: str, variables: Dict[str, ValidScxmlTypes]
+) -> Union[ValidScxmlTypes, js2py.base.JsObjectWrapper]:
     """
-    Interpret the ECMA script expression.
+    Process a JS expression and return the resulting outcome.
 
-    :param expr: The ECMA script expression
-    :return: The interpreted object
+    :param expr: The ECMA script expression to evaluate.
+    :param variables: A dictionary of variables to be used in the ECMA script context.
     """
-    if variables is None:
-        variables = {}
     context = js2py.EvalJs(variables)
     try:
         context.execute("result = " + expr)
@@ -60,10 +58,7 @@ def interpret_ecma_script_expr(
             else:
                 return array("d", res_as_list)
         else:
-            raise ValueError(
-                f"Expected expr. {expr} to be of type {BasicJsTypes} or "
-                f"an array, got '{type(expr_result._obj)}'"
-            )
+            return expr_result
     elif isinstance(expr_result, array):
         return expr_result
     else:
@@ -71,3 +66,39 @@ def interpret_ecma_script_expr(
             f"Expected expr. {expr} to be of type {BasicJsTypes} or "
             f"JsObjectWrapper, got '{type(expr_result)}'"
         )
+
+
+def interpret_ecma_script_expr(
+    expr: str, variables: Optional[Dict[str, ValidScxmlTypes]] = None
+) -> ValidScxmlTypes:
+    """
+    Interpret the ECMA script expression. Return it only if result compatible with plain SCXML.
+
+    :param expr: The ECMA script expression
+    :param variables: A dictionary of variables to be used in the ECMA script context
+    :return: The interpreted object
+    """
+    if variables is None:
+        variables = {}
+    expr_result = _interpret_ecmascript_expr(expr, variables)
+    if isinstance(expr_result, js2py.base.JsObjectWrapper):
+        raise ValueError(
+            f"Expected expr. {expr} to be of type {BasicJsTypes} or "
+            f"an array, got '{type(expr_result._obj)}'"
+        )
+    return expr_result
+
+
+def interpret_non_base_ecma_script_expr(
+    expr: str, variables: Optional[Dict[str, ValidScxmlTypes]] = None
+) -> Union[ValidScxmlTypes, js2py.base.JsObjectWrapper]:
+    """
+    Interpret the ECMA script expression. Returns also complex objects.
+
+    :param expr: The ECMA script expression
+    :param variables: A dictionary of variables to be used in the ECMA script context
+    :return: The interpreted object
+    """
+    if variables is None:
+        variables = {}
+    return _interpret_ecmascript_expr(expr, variables)
---
+++ src/as2fm/scxml_converter/scxml_data_types/scxml_data_struct.py
@@ -0,0 +1,113 @@
+# Copyright (c) 2025 - for information on the respective copyright owner
+# see the NOTICE file
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, Dict, Optional, Union
+
+from lxml.etree import _Element as XmlElement
+
+# from as2fm.as2fm_common.ecmascript_interpretation import interpret_non_base_ecma_script_expr
+from as2fm.as2fm_common.logging import get_error_msg
+from as2fm.scxml_converter.scxml_entries.utils import SCXML_DATA_STR_TO_TYPE
+
+# from as2fm.scxml_converter.scxml_entries.xml_utils import get_xml_attribute
+
+ExpandedDataStructType = Dict[str, Union[str, Dict[str, "ExpandedDataStructType"]]]
+
+
+class ScxmlDataStruct:
+    """
+    Represents a custom data type defined in SCXML, loaded from an XML element.
+    """
+
+    def __init__(self, name: str, members: dict):
+        """
+        Initializes the ScxmlDataType instance.
+
+        :param name: The name of the custom data type.
+        :param members: A dictionary where keys are member names and values are their types.
+        """
+        self.name = name
+        self.members: Dict[str, str] = members
+        # This needs to be generated once all struct definitions are loaded.
+        # Each key can be linked to a base type or another dictionary, containing other subfields
+        self._expanded_members: Optional[ExpandedDataStructType] = None
+
+    @classmethod
+    def from_xml(cls, xml_element: XmlElement):
+        """
+        Creates an ScxmlDataType instance from an `struct` XML element.
+
+        :param xml_element: The XML struct element defining the custom data type.
+        :return: An instance of ScxmlDataType.
+        """
+        name = xml_element.get("id")
+        members = {}
+        for member in xml_element.findall("member"):
+            member_name = member.get("id")  # get_xml_attribute
+            member_type = member.get("type")
+            assert member_name and member_type, get_error_msg(
+                xml_element, "Member with no id or type defined."
+            )
+            members[member_name] = member_type
+        assert len(members) > 0, get_error_msg(xml_element, "struct definition with no members.")
+        return cls(name, members)
+
+    def get_expanded_members(self) -> ExpandedDataStructType:
+        """
+        Retrieve the expanded members of the data structure.
+        """
+        assert self._expanded_members is not None
+        return self._expanded_members
+
+    def expand_members(self, all_structs: Dict[str, "ScxmlDataStruct"]):
+        """
+        Expands the members dictionary to resolve all fields to their base types.
+
+        :param all_structs: A dictionary of all ScxmlDataStruct instances by their names.
+        """
+        if self._expanded_members is not None:
+            return  # Already expanded
+
+        self._expanded_members = {}
+        for member_name, member_type in self.members.items():
+            if member_type in SCXML_DATA_STR_TO_TYPE:
+                # Base type, directly map it
+                self._expanded_members[member_name] = member_type
+            elif member_type in all_structs:
+                # Reference to another struct, recursively expand it
+                all_structs[member_type].expand_members(all_structs)
+                self._expanded_members[member_name] = all_structs[
+                    member_type
+                ].get_expanded_members()
+            else:
+                raise ValueError(
+                    f"Unknown type '{member_type}' for member "
+                    f"'{member_name}' in struct '{self.name}'."
+                )
+
+    def get_instance_from_expression(self, expr: str) -> Dict[str, Any]:
+        """
+        Creates an instance of the data structure from an ECMAScript-like expression.
+
+        :param expr: The expression defining the instance.
+        :return: A dictionary representing the instance.
+        """
+        if self._expanded_members is None:
+            raise ValueError(f"Struct '{self.name}' has not been expanded yet.")
+
+        # Interpret the expression
+        # interpreted_expr = interpret_non_base_ecma_script_expr(expr)
+        # TODO: All
+        return {}
