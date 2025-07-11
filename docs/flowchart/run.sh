cca91b5ccc872c09a81e0dd23dfc1f4cd4767546
Christian Henkel <christian.henkel2@de.bosch.com>
2025-07-11 10:13:14 +0200

    smaller
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- docs/flowchart/run.sh
+++ docs/flowchart/run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/sh
 AS2FM_PKG=../../src/as2fm
-../../src/as2fm
+
 code2flow \
-$AS2FM_PKG/jani_generator/main.py \
-$AS2FM_PKG/jani_generator/scxml_helpers/top_level_interpreter.py \
-$AS2FM_PKG/jani_generator/scxml_helpers/scxml_to_jani.py \
-$AS2FM_PKG/jani_generator/scxml_helpers/scxml_event_processor.py \
-$AS2FM_PKG/jani_generator/scxml_helpers/scxml_to_jani_interfaces.py \
-$AS2FM_PKG/jani_generator/ros_helpers/ros_communication_handler.py \
-$AS2FM_PKG/jani_generator/jani_entries/jani_helpers.py \
-$AS2FM_PKG/scxml_converter/scxml_entries/scxml_root.py \
-$AS2FM_PKG/as2fm_common/ecmascript_interpretation.py
+-o main.png \
+--target-function main_scxml_to_jani \
+--upstream-depth 2 \
+--downstream-depth 2 \
+$AS2FM_PKG/*
