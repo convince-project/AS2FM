fdcf34797b658f2a7181b4580517e94c29c1c0b2
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2025-08-01 15:55:03 +0200

    Remove some leftover string handling in JANI
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- src/as2fm/scxml_converter/data_types/type_utils.py
+++ src/as2fm/scxml_converter/data_types/type_utils.py
@@ -15,9 +15,9 @@
 
 import re
 from dataclasses import dataclass
-from typing import Any, Dict, List, MutableSequence, Optional, Tuple, Type, Union, get_args
+from typing import Any, Dict, List, MutableSequence, Optional, Tuple, Type, Union
 
-from as2fm.as2fm_common.common import ValidPlainScxmlTypes, get_array_type_and_sizes
+from as2fm.as2fm_common.common import get_array_type_and_sizes
 from as2fm.as2fm_common.ecmascript_interpretation import interpret_ecma_script_expr
 
 # TODO: add lower and upper bounds depending on the n. of bits used.
@@ -181,45 +181,3 @@ def get_array_info(data_type: str, expect_base_type: bool = True) -> ArrayInfo:
         array_py_type = SCXML_DATA_STR_TO_TYPE[array_type_str]
         return ArrayInfo(array_py_type, n_dims, array_max_sizes, expect_base_type)
     return ArrayInfo(array_type_str, n_dims, array_max_sizes, expect_base_type)
-
-
-def check_variable_base_type_ok(
-    data_value: ValidPlainScxmlTypes,
-    expected_data_type: Type[ValidPlainScxmlTypes],
-    array_info: Optional[ArrayInfo] = None,
-) -> bool:
-    """
-    Checks if the given data value matches the expected data type (and the opt. array information).
-
-    This function is to be used only on base types and arrays of those.
-    :param data_value: The value to be checked, expected to be of a valid SCXML type.
-    :param expected_data_type: The expected type of the data value.
-    :param array_info: Information about the array, if the data value is expected to one.
-    :return: True if the data value matches the expected type, otherwise False.
-    """
-    valid_types = get_args(ValidPlainScxmlTypes)
-    assert (
-        expected_data_type in valid_types
-    ), f"Invalid expected data type '{expected_data_type}' not in {valid_types}"
-    expected_types: Tuple[Type, ...] = (expected_data_type,)
-    if isinstance(data_value, valid_types):
-        if isinstance(data_value, MutableSequence):
-            assert array_info is not None
-            # TODO: Small hack to accept integer values in case array type is float
-            expected_types = (int,) if array_info.array_type is int else (int, float)
-
-            # We are dealing with a list, use array_info data
-            def recurse_on_array(
-                data_value: MutableSequence, dims_left: int, base_types: Tuple[Type, ...]
-            ) -> bool:
-                assert dims_left > 0
-                if dims_left == 1:
-                    return all(isinstance(entry, base_types) for entry in data_value)
-                return all(
-                    recurse_on_array(entry, dims_left - 1, base_types) for entry in data_value
-                )
-
-            return recurse_on_array(data_value, array_info.array_dimensions, expected_types)
-        else:
-            return isinstance(data_value, expected_types)
-    return False
